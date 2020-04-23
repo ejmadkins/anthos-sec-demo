2aa2be4fbe2122ed2f1b3a3f05ffc22f8b186ea8
Ed Adkins <ejmadkins@ejmadkins-macbookpro.roam.corp.google.com>
2020-04-23 11:14:34 +0100

    fix function name

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def t_rex_runner():
-    return render_template('index.html', hostname=get_cluster_name_metadata())
+    return render_template('index.html', hostname=hostname_metadata())
 
 def hostname_metadata():
     return requests.get("http://metadata/computeMetadata/v1/instance/name",
